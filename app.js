f0ad1be59daf19e9542f0aae519bea3a9171d442
Joyce Santos <joycessilva23@hotmail.com>
2025-05-24 13:42:58 -0300

    Conceitos js console e passando referencias

---
+++ app.js
@@ -0,0 +1,11 @@
+// Criando uma varivel chamada nome e atribuindo um valor a ela
+//var nome = "Matheus"
+
+// chamando console.log
+//console.log(nome)
+
+
+//Criando uma variavel para definir idade e chamando o prompt
+//var idade = prompt("Digite Sua Idade");
+
+//quando chamamos document estamos nos referindo a parte do nosso arquivo
